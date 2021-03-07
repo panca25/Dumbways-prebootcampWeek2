6d202ca3384a4f47fa24cf72036008b15ad2e63d
Panca Setiawan <pancasetiawan225@gmail.com>
2021-03-08 01:21:21 +0700

    Add files via upload

---
+++ task7.js
@@ -0,0 +1,34 @@
+let input1 = ["a","b","c","d","e"]; 
+let input2 = "c";
+
+for(let i = 0; i < input1.length; i++){
+    let printOutput = [];
+    if (input1[i] !== input2){
+        printOutput.push(input1[i]);
+    }
+    document.write(printOutput);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let input2 = prompt("Enter input-2");
+
+// document.write(input2);
